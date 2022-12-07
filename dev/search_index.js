89d54eff25b168fa6d6ba58010d7e4eda7f3cb09
Documenter.jl <documenter@juliadocs.github.io>
2022-12-07 13:31:14 +0000

    build based on c92b328

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PIQL","category":"page"},{"location":"#PIQL","page":"Home","title":"PIQL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PIQL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PIQL]","category":"page"}]
+}
