e5ef0916cd052f5edb12430b09bec40a3b3b064d
chelate <42802644+chelate@users.noreply.github.com>
2023-01-25 09:08:48 +0100

    fixed endpoint of cost

--- src/analysis_tools.jl
+++ src/analysis_tools.jl
@@ -7,10 +7,18 @@ Monte Carlo estimate for the performance of our state.
 function performance_estimate(ctrl, actor; state = ctrl.initial_state(), sa = initial_action(state, ctrl, actor))
     control_cost = 0.0
     state_cost = 0.0
-    while !ctrl.terminal_condition(sa.state)
+    while true
         state_cost += sa.cost
-        control_cost += sa.β * (sa.u - sa.f)
-        sa = new_state_action(sa, ctrl, actor)
+        control_cost += sa.β * (sa.f - sa.u)
+        if ctrl.terminal_condition(sa.state)
+            break
+        else
+            sa = new_state_action(sa, ctrl, actor)
+        end
     end
     return (state_cost, control_cost)
 end
+
+"""
+S = -<F-E> = U - F
+"""
